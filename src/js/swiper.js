7e0393c953df786846cd74e261fd0c292f407882
hector <hectorperez1238@gmail.com>
2022-09-29 11:11:34 -0400

    swiper deskto listo

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -5,9 +5,13 @@ let isMovil = false;
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 10,
-    slidesPerGroup:1,
+    slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
