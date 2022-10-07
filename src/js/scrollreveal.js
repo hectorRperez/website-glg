232841da3a4d9c82f6ab9246a46abae0bccc8c95
hector <hectorperez1238@gmail.com>
2022-10-07 11:37:37 -0400

    implementación de scrollreveal

---
+++ src/js/scrollreveal.js
@@ -0,0 +1,5 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.headline', { delay: 400 });
+ScrollReveal().reveal('.client', { delay: 400 });
+ScrollReveal().reveal('.logistic__section', { delay: 400 });
+ScrollReveal().reveal('.logistic-swiper', { delay: 400 });
