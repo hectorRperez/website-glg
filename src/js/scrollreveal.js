21c0822555aee50bd0c054c0f1e94cedbe0ce5f8
hector <hectorperez1238@gmail.com>
2022-10-12 13:49:34 -0400

    test video

--- src/js/scrollreveal.js
+++ src/js/scrollreveal.js
@@ -3,3 +3,4 @@ ScrollReveal().reveal('.headline', { delay: 400 });
 ScrollReveal().reveal('.client', { delay: 400 });
 ScrollReveal().reveal('.logistic__section', { delay: 400 });
 ScrollReveal().reveal('.logistic-swiper', { delay: 400 });
+ScrollReveal().reveal('.carriers', { delay: 400 });
