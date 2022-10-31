a89db1a723413b66e3fed30feda8121c08a0f89a
hector <hectorperez1238@gmail.com>
2022-10-31 12:46:14 -0400

    seccion de carriers revisa y aceptada

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,11 +1,16 @@
 var nav = document.querySelector('.navbar')
 var welcomeTitle = document.querySelector('.welcome__smallTitle')
+var test__img = document.querySelector('.test__img');
+
+console.log(test__img);
+
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 100) {
         nav.classList.add('bg-dark-scroll', 'shadow', 'p-2')
     } else {
-        nav.classList.remove('bg-dark-scroll', 'shadow', 'p-2')
+        test__img.src = "/src/img/Capa 1.png";
+        nav.classList.remove('bg-dark-scroll', 'shadow', 'p-3')
     }
 })
 
