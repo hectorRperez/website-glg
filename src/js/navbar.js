043be057a61e0343395710e89186363ba3af3d25
hector <hectorperez1238@gmail.com>
2022-11-01 13:57:29 -0400

    carriers section ready

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,16 +1,11 @@
 var nav = document.querySelector('.navbar')
 var welcomeTitle = document.querySelector('.welcome__smallTitle')
-var test__img = document.querySelector('.test__img');
-
-console.log(test__img);
-
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 100) {
         nav.classList.add('bg-dark-scroll', 'shadow', 'p-2')
     } else {
-        test__img.src = "/src/img/Capa 1.png";
-        nav.classList.remove('bg-dark-scroll', 'shadow', 'p-3')
+        nav.classList.remove('bg-dark-scroll', 'shadow', 'p-2')
     }
 })
 
